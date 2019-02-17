13c7718d3a09ef2bedb548e212476957bc5d2a96
Emily Emerson <emily.emerson@gmail.com>
2019-02-17 20:51:18 +0000

    Automatically backed up by Learn

--- badges.rb
+++ badges.rb
@@ -1,4 +1,6 @@
 def badge_maker
   intro = "hello! my name is [name]"
   speakers = [Edgar, Ada, Charles, Alan, Grace, Linus, Matz]
-  rooms = [1,2,3,4,5,6,7]
+  room_numbers = [1,2,3,4,5,6,7]
+  room_assignments = "hello, [name]! you'll be assigned to room [room_numbers]"
+  
